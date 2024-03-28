487cc81ba2a893773150d5efc88097cab8fe216c
BBO <benjamin.bouachour@bioserenity.com>
2024-03-28 17:59:39 +0100

    add: filepath to config the plugin file output

--- plugins/files/rawfile/plugin.go
+++ plugins/files/rawfile/plugin.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"os"
+	"path/filepath"
 
 	"github.com/benji-bou/SecPipeline/helper"
 	"github.com/benji-bou/SecPipeline/pluginctl"
@@ -13,21 +15,40 @@ import (
 
 type RawFileOption = helper.Option[RawFile]
 
-type RawFile struct{}
+type ConfigRawFile struct {
+	Filepath string `json:"filepath"`
+}
 
-func NewRawFile(opt ...RawFileOption) RawFile {
-	return helper.Configure(RawFile{}, opt...)
+type RawFile struct {
+	config ConfigRawFile
+}
+
+func NewRawFile(opt ...RawFileOption) *RawFile {
+	return helper.ConfigurePtr(&RawFile{}, opt...)
 }
 func (mp RawFile) GetInputSchema() ([]byte, error) {
 	return nil, nil
 }
 
-func (mp RawFile) Config([]byte) error {
-
+func (mp *RawFile) Config(config []byte) error {
+	configRawFile := ConfigRawFile{}
+	err := json.Unmarshal(config, &configRawFile)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal json for RawFile config: %w", err)
+	}
+	mp.config = configRawFile
 	return nil
 }
 func (mp RawFile) Run(ctx context.Context, input <-chan *pluginctl.DataStream) (<-chan *pluginctl.DataStream, <-chan error) {
-	f, err := os.OpenFile("result.txt", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	basePath, err := os.UserHomeDir()
+	if err != nil {
+		basePath = "./"
+	}
+	defaultFilePath := filepath.Join(basePath, ".secpipeline", "result.txt")
+	if mp.config.Filepath != "" {
+		defaultFilePath = mp.config.Filepath
+	}
+	f, err := os.OpenFile(defaultFilePath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
 		return nil, chantools.Once(fmt.Errorf("NewRawFile open file for  plugin failed, %w", err))
 	}
